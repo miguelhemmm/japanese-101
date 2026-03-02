d72da02cb9b86b0c93fb73a3567abebb47580500
AWSReservedSSO_COMS-RW-NPRD_3e89a37f8b1c3f83/miguel.hernandez@acmconnect.com <miguel.hernandez@acmconnect.com>
2026-03-02 16:14:59 -0600

    fixed some styles

--- src/features/selection/components/CategoryCard/CategoryCard.js
+++ src/features/selection/components/CategoryCard/CategoryCard.js
@@ -1,7 +1,13 @@
-import { useTranslation } from 'react-i18next';
-import './CategoryCard.css';
+import { useTranslation } from "react-i18next";
+import "./CategoryCard.css";
 
-export const CategoryCard = ({ category, info, isSelected, onToggle, onStudy }) => {
+export const CategoryCard = ({
+  category,
+  info,
+  isSelected,
+  onToggle,
+  onStudy,
+}) => {
   const { t } = useTranslation();
 
   const handleStudyClick = (e) => {
@@ -10,25 +16,27 @@ export const CategoryCard = ({ category, info, isSelected, onToggle, onStudy }) 
   };
 
   return (
-    <div className={`category-card ${isSelected ? 'selected' : ''}`}>
-      <button
-        className="category-card__select"
-        onClick={() => onToggle(category)}
-        type="button"
-      >
-        <span className="category-icon">{info.icon}</span>
-        <span className="category-name">{t(info.nameKey)}</span>
-        <span className="category-description">{t(info.descriptionKey)}</span>
-        <span className="category-checkbox">
-          {isSelected ? '✓' : ''}
-        </span>
-      </button>
+    <div className="category-card-container">
+      <div className={`category-card ${isSelected ? "selected" : ""}`}>
+        <button
+          className="category-card__select"
+          onClick={() => onToggle(category)}
+          type="button"
+        >
+          <span className="category-icon">{info.icon}</span>
+          <span className="category-name">{t(info.nameKey)}</span>
+          <span className="category-description">{t(info.descriptionKey)}</span>
+          <span className="category-checkbox">
+            <i className="fa-solid fa-check"></i>
+          </span>
+        </button>
+      </div>
       <button
         className="category-card__study-button"
         onClick={handleStudyClick}
         type="button"
       >
-        {t('buttons.study')}
+        {t("buttons.study")}
       </button>
     </div>
   );
