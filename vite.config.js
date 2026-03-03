a77b24a26d817a94c8b3915c95e1673ab58542b3
AWSReservedSSO_COMS-RW-NPRD_3e89a37f8b1c3f83/miguel.hernandez@acmconnect.com <miguel.hernandez@acmconnect.com>
2026-03-03 10:45:02 -0600

    migrated to vite - fix config for cloudfare pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/japanese-101/' : '/',
+  base: '/',
   esbuild: {
     loader: 'jsx',
     include: /src\/.*\.js$/,
